b58c56d2062221cbb2af18b1e3c9cc0fd985e784
abeshi03 <k_913411@yahoo.co.jp>
2022-08-27 22:50:19 +0900

    fix proto

--- server/codegen/protos/user_grpc_pb.js
+++ server/codegen/protos/user_grpc_pb.js
@@ -26,10 +26,43 @@ function deserialize_UserResponse(buffer_arg) {
   return protos_user_pb.UserResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_UsersRequest(arg) {
+  if (!(arg instanceof protos_user_pb.UsersRequest)) {
+    throw new Error('Expected argument of type UsersRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_UsersRequest(buffer_arg) {
+  return protos_user_pb.UsersRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_UsersResponse(arg) {
+  if (!(arg instanceof protos_user_pb.UsersResponse)) {
+    throw new Error('Expected argument of type UsersResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_UsersResponse(buffer_arg) {
+  return protos_user_pb.UsersResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 
 var UserManagerService = exports.UserManagerService = {
-  get: {
-    path: '/UserManager/get',
+  getUsers: {
+    path: '/UserManager/GetUsers',
+    requestStream: false,
+    responseStream: false,
+    requestType: protos_user_pb.UsersRequest,
+    responseType: protos_user_pb.UsersResponse,
+    requestSerialize: serialize_UsersRequest,
+    requestDeserialize: deserialize_UsersRequest,
+    responseSerialize: serialize_UsersResponse,
+    responseDeserialize: deserialize_UsersResponse,
+  },
+  getUser: {
+    path: '/UserManager/GetUser',
     requestStream: false,
     responseStream: false,
     requestType: protos_user_pb.UserRequest,
